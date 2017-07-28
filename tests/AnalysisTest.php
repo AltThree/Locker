20f6980925f41ce6c62eb5b1375614318e48d67a
Graham Campbell <graham@alt-three.com>
2017-07-28 16:32:57 +0100

    Excluded another class

--- tests/AnalysisTest.php
+++ tests/AnalysisTest.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace AltThree\Tests\Locker;
 
 use GrahamCampbell\Analyzer\AnalysisTrait;
+use Illuminate\Redis\Connections\Connection;
 use Laravel\Lumen\Application;
 use PHPUnit\Framework\TestCase;
 
@@ -47,6 +48,6 @@ class AnalysisTest extends TestCase
      */
     protected function getIgnored()
     {
-        return [Application::class];
+        return [Application::class, Connection::class];
     }
 }
